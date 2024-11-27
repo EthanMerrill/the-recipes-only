54f040ec588dfb5d56830d999e1ca3bc79ee002d
Ethan <eth22mer@gmail.com>
2024-11-27 13:23:08 -0500

    Update openai API for compatibility

--- src/pages/api/getRecipe.ts
+++ src/pages/api/getRecipe.ts
@@ -7,25 +7,33 @@ import axios from "axios";
 // it takes a prompt and returns the completion
 
 export const simpleRecipeApi = (promptText: string) => {
-  console.log(`Making a query to the API with the prompt: ${promptText}`)
-  const client = axios.create({headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
-  }})
+  console.log(`Making a query to the API with the prompt: ${promptText}`);
+  const client = axios.create({
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+    },
+  });
 
   const params = {
-    'model': 'text-davinci-003',
-    'prompt': promptText,
-    'max_tokens': 500,
-    'temperature': 0.5,
-    'top_p': 1,
-    'frequency_penalty': 0,
-    'presence_penalty': 0,
-  }
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a recipe bot.",
+      },
+      {
+        role: "user",
+        content: promptText,
+      },
+    ],
+    // max_tokens: 500,
+    // temperature: 0.5,
+    // top_p: 1,
+    // frequency_penalty: 0,
+    // presence_penalty: 0,
+  };
 
-  const data = client.post(
-    "https://api.openai.com/v1/completions", 
-    params
-  )
-  return data
-}
+  const data = client.post("https://api.openai.com/v1/chat/completions", params);
+  return data;
+};
--- src/pages/recipe/NewRecipe.tsx
+++ src/pages/recipe/NewRecipe.tsx
@@ -9,20 +9,20 @@ import { simpleRecipeApi } from "../api/getRecipe";
 import { useRouter } from "next/router";
 import { AppContext } from "@/context/state";
 import IngredientsInstructions from "@/components/IngredientsInstructions";
-import {useQuery } from "@tanstack/react-query"
+import { useQuery } from "@tanstack/react-query"
 import { serverTimestamp } from "firebase/firestore"
 
 // const fetcher = (recipe:any) => simpleRecipeApi(recipe).then(({data}) => data);
 
-export default function NewRecipe () {
-    
+export default function NewRecipe() {
+
     const [recipe, setRecipe] = useState({} as Recipe)
     // const [searchTerm, setSearchTerm] = useState('')
     const router = useRouter()
     let searchTerm = router.query.searchTerm
     const appContext = useContext(AppContext)
 
-    let prompt = encodeURI(`${searchTerm} recipe with ingredients and instructions only`)
+    let prompt = encodeURI(`${searchTerm} recipe with ingredients and instructions only please include an 'Ingredients:' and 'Instructions:' header`)
     // API QUERY
     const { isLoading, error, data, isFetching, refetch } = useQuery({
         queryKey: ["openAI"],
@@ -30,12 +30,12 @@ export default function NewRecipe () {
         refetchOnWindowFocus: false,
         enabled: false, // disable this query from automatically running
         queryFn: () =>
-        simpleRecipeApi(prompt)
-            .then((res) => res.data),
-      });
+            simpleRecipeApi(prompt)
+                .then((res) => res.data),
+    });
 
     /// END API QUERY
-    
+
     useEffect(() => {
         console.log('data', data, 'isFetching', isFetching, 'isLoading', isLoading, 'error', error)
     }, [data, error, isFetching, isLoading])
@@ -45,8 +45,8 @@ export default function NewRecipe () {
     // set the name of the recipe every time the search term changes & update the state
     useEffect(() => {
         console.log('set the name of the recipe every time the search term changes & update the state')
-        if(router.query.searchTerm){
-            setRecipe({name: capitalizeFirstLetter(router.query.searchTerm) as string, ingredients: [], instructions: []})
+        if (router.query.searchTerm) {
+            setRecipe({ name: capitalizeFirstLetter(router.query.searchTerm) as string, ingredients: [], instructions: [] })
             // update the state with the properly formatted recipe name
             appContext.setRecipeName(recipe.name)
         }
@@ -55,34 +55,35 @@ export default function NewRecipe () {
     // refetch the recipe from openAI every time the search term changes
     useEffect(() => {
         console.log('refetch the recipe from openAI every time the search term changes')
-        if(searchTerm){
+        if (searchTerm) {
             refetch()
-            }
+        }
     }, [searchTerm])
 
     useEffect(() => {
-        console.log('set recipe', data?.choices[0].text)
-        if(!isFetching && !isLoading && ! isFetching && data?.choices[0].text){
-            setRecipe(recipeFormatter(searchTerm as string, data?.choices[0].text))
+        console.log('set recipe', data?.choices[0].message.content)
+        if (!isFetching && !isLoading && !isFetching && data?.choices[0].message.content) {
+            setRecipe(recipeFormatter(searchTerm as string, data?.choices[0].message.content))
         }
     }, [searchTerm, isFetching, isLoading, data])
 
     // save the recipe to firestore
     useEffect(() => {
         console.log('save the recipe to firestore')
-        if(recipe.name && recipe.ingredients && recipe.instructions && recipe.ingredients.length > 0 && recipe.instructions.length > 0){
+        if (recipe.name && recipe.ingredients && recipe.instructions && recipe.ingredients.length > 0 && recipe.instructions.length > 0) {
             // save recipe to firestore db
-            setDoc(doc(db, "recipes", recipe.name), {...recipe, 
+            setDoc(doc(db, "recipes", recipe.name), {
+                ...recipe,
                 created: serverTimestamp()
-              })
-        }   
+            })
+        }
         // else {
         //     console.log('no recipe to save')
         //     alert('sorry there was an error creating your recipe. Please try again later.')
         // }
     }, [appContext, recipe, recipe.name])
 
-    return(
+    return (
         <>
             <Head>
                 <title>The Recipes Only</title>
@@ -90,12 +91,12 @@ export default function NewRecipe () {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-        <Header/>
+            <Header />
             <main className='flex flex-col p-6 min-h-screen  bg-page-bg dark:bg-gray-dark'>
                 <div className='border-t border-gray-50 py-1'></div>
                 <h1 className="font-serif text-3xl sm:w-3/5 w-full mx-auto pt-5 text-center">{searchTerm}</h1>
                 <Suspense fallback={<div>Loading...</div>}>
-                <IngredientsInstructions loading={isLoading || isFetching} ingredients={recipe.ingredients} instructions={recipe.instructions} />
+                    <IngredientsInstructions loading={isLoading || isFetching} ingredients={recipe.ingredients} instructions={recipe.instructions} />
                 </Suspense>
             </main>
         </>
